a5f60fc53f937dfa5e3cf1e0d38a7206246d20f4
Guewenhtbs <kerguegue@gmail.com>
2023-12-22 14:36:19 +0100

    (création_classe): affichage satisfaisant

--- automatisation.py
+++ automatisation.py
@@ -17,15 +17,18 @@ class ROUTER :
         self.neighbors = neighbors_list
         self.border = border_list
         self.as_n = as_n
-        self.id = (f"{self.as_n}.{self.as_n}.{self.as_n}.{self.as_n}")
+        self.id = (f"{self.name}.{self.name}.{self.name}.{self.name}")
 
     def __str__(self):
         return (f"Le nom du routeur est R{self.name}, les adresses ip sont {self.ip}, qui sont associés aux voisins {self.neighbors}, le fait d'être un routeur de bordure est défini par {self.border}, son id BGP est {self.id}.")
 
+    def __repr__(self):
+        return (f"R{self.name}")
+
 
 def main():
     sfr = AS("11111","OSPF",["22222"])
-    marbella = ROUTER("1",["3500:1::1/64",None,None,None,"3500:3500::1/128"],["2",None,None,None],[0],None)
+    marbella = ROUTER(1,["3500:1::1/64",None,None,None,"3500:3500::1/128"],[2,None,None,None],[0],None)
     marbella.as_n = sfr.number
     sfr.router.append(marbella)
     print(sfr)
