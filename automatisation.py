4cf7b528883b25a96a3af91af20b0259ea5603bd
Guewenhtbs <kerguegue@gmail.com>
2024-01-09 15:51:29 +0100

    (BGP): fonction search_id et finition de la boucle pour les routeurs

--- automatisation.py
+++ automatisation.py
@@ -46,7 +46,7 @@ def lecture_json():
     return liste_AS
 
 def BGP(r_name,AS,r_id,neighbors_BGP,prefixes):
-    fichier = open(f"R{r_name}.txt","a")
+    fichier = open(f"./config_test/R{r_name}.txt","a")
     fichier.write(f"\nrouter bgp {AS}\n bgp router-id {r_id}\n bgp log-neighbor-changes\n no bgp default ipv4-unicast")
     for (ip,v_AS) in neighbors_BGP :
         if v_AS == AS :
@@ -61,6 +61,17 @@ def BGP(r_name,AS,r_id,neighbors_BGP,prefixes):
     fichier.write("\n exit-address-family\n!\nipforward-protocol nd\n!\n!\nno ip http server\nno ip http secure-server\n")
     fichier.close()
 
+def search_ip(r_name,v_name,liste_AS,AS_n):
+    for As in liste_AS :
+        if As.number == AS_n :
+            for router in As.router :
+                if router.name == v_name :
+                    for index in range(4):
+                        if router.neighbors[index] == r_name :
+                            return router.ip_address[index]
+    print("IP non trouvé")
+    return None
+
 
 ##############MAIN#############
 liste_AS = lecture_json()
@@ -74,6 +85,9 @@ for As in liste_AS :
             for i in range(router.border[0]) :
                 if router.border[i*2+1] == index :
                     neighbor_bgp.append((search_ip(neighbor),router.border[i*2+2]))
+            for v_router in As.router :
+                if router != router :
+                    neighbor_bgp.append((v_router.ip_address[4],As.number))
     
 
         BGP(router.name,As.number,router.id,)
