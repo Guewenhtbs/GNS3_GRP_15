e2caf00a37185104e3ccc13c458eda258892bc52
Allisson22 <allisson.roussel@insa-lyon.fr>
2024-01-10 22:45:27 +0100

    (deplacement_fichiers_config) : fichier déplacé correctement pour tous les routeurs

--- automatisation.py
+++ automatisation.py
@@ -1,7 +1,5 @@
 import json
 from datetime import datetime
-import os
-import uuid
 
 with open("Donnees.json",'r',encoding='utf-8') as f :
     data = json.load(f)
@@ -50,40 +48,45 @@ def lecture_json():
 def debut(fichier, name):
     now = datetime.utcnow()
     formatted_date = now.strftime("%H:%M:%S UTC %a %b %d %Y")
-    fichier.write("!\n\n!\n! Last configuration change at " + formatted_date + "\n!\nversion 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\n!\nhostame R" + str(name) + "\n!\nboot-start-marker\nboot-end-marker\n!\n!\n!\nno aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n!\n!\n!\n!\n!\n!\nno ip domain lookup\nipv6 unicast-routing\nipv6 cef\n!\n!\nmultilink bundle-name authenticated\n!\n!\n!\n!\n!\n!\n!\n!\n!\nip tcp synwait-time 5\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!")
+    fichier.write("!\n\n!\n! Last configuration change at " + formatted_date + "\n!\nversion 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\n!\nhostname R" + str(name) + "\n!\nboot-start-marker\nboot-end-marker\n!\n!\n!\nno aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n!\n!\n!\n!\n!\n!\nno ip domain lookup\nipv6 unicast-routing\nipv6 cef\n!\n!\nmultilink bundle-name authenticated\n!\n!\n!\n!\n!\n!\n!\n!\n!\nip tcp synwait-time 5\n! \n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!")
 
-def interfaces(fichier, ip, igp, name):
+def interfaces(fichier, ip, igp,interface_border):
     fichier.write(f"\ninterface Loopback0\n no ip address\n ipv6 address {ip[4]}/128\n ipv6 enable")
     ospf_ou_rip(igp, fichier)
 
     nom_interface = ["FastEthernet0/0", "GigabitEthernet1/0", "GigabitEthernet2/0", "GigabitEthernet3/0"]
 
-    fichier.write(f"\ninterface {nom_interface[0]}")
+    fichier.write(f"\n!\ninterface {nom_interface[0]}")
     if ip[0] is None:
         fichier.write("\n no ip address\n shutdown\n duplex full\n!")
     else:
         fichier.write(f"\n no ip address\n duplex full\n ipv6 address {ip[0]}/64\n ipv6 enable")
-        ospf_ou_rip(igp, fichier)
-
+        if 1 not in interface_border or igp == "OSPF":
+            ospf_ou_rip(igp, fichier)
+        fichier.write("\n!")
     for i in range(1, len(nom_interface)):
         fichier.write(f"\ninterface {nom_interface[i]}")
         if ip[i] is None:
             fichier.write("\n no ip address\n shutdown\n negotiation auto\n!")
         else:
-            aux_interfaces(fichier, ip[i], igp)
+            border = False
+            if i+1 in interface_border :
+                border = True
+            aux_interfaces(fichier, ip[i], igp,border)
 
-def aux_interfaces(fichier, adresse, igp):
+def aux_interfaces(fichier, adresse, igp, border):
     fichier.write(f"\n no ip address\n negotiation auto\n ipv6 address {adresse}/64\n ipv6 enable")
-    ospf_ou_rip(igp, fichier)
+    if border == False or igp == "OSPF":
+        ospf_ou_rip(igp, fichier)
+    fichier.write("\n!")
 
 def ospf_ou_rip(igp, fichier):
     if igp == "OSPF":
-        fichier.write(f"\n ipv6 ospf 15 area 0\n!")
+        fichier.write(f"\n ipv6 ospf 15 area 0")
     elif igp == "RIP":
-        fichier.write(f"\n ipv6 rip 15 enable\n!")
+        fichier.write(f"\n ipv6 rip 15 enable")
         
-def BGP(r_name,AS,r_id,neighbors_BGP,prefixes) :
-    fichier = open(f"Config_test/R{r_name}.txt","w")
+def BGP(fichier,AS,r_id,neighbors_BGP,prefixes) :
     fichier.write(f"\nrouter bgp {AS}\n bgp router-id {r_id}\n bgp log-neighbor-changes\n no bgp default ipv4-unicast")
     border = False
     for (ip,v_AS) in neighbors_BGP :
@@ -98,18 +101,16 @@ def BGP(r_name,AS,r_id,neighbors_BGP,prefixes) :
             fichier.write(f"\n  network {pref}")
     for (ip,_) in neighbors_BGP :
         fichier.write(f"\n  neighbor {ip} activate")
-    fichier.write("\n exit-address-family\n!\nipforward-protocol nd\n!\n!\nno ip http server\nno ip http secure-server\n!")
-    fichier.close()
+    fichier.write("\n exit-address-family\n!\nip forward-protocol nd\n!\n!\nno ip http server\nno ip http secure-server\n!")
 
-def IGP(r_name,r_id,igp,passive) :
-    fichier = open(f"Config_test/R{r_name}.txt","a")
+def IGP(fichier,r_id,igp,passive) :
     if igp == "RIP" :
-        fichier.write(f"\nipv6 routeur rip 15\n redistribute connected")
+        fichier.write(f"\nipv6 router rip 15\n redistribute connected")
     elif igp == "OSPF" :
         fichier.write(f"\nipv6 router ospf 15\n router-id {r_id}")
         for interface in passive :
-            fichier.write(f"\npassive-interface {interface}")
-    fichier.write("\n!\n!\n!\n!\ncontrol-plane\n!\n!\nline con 0\n exec-timeout 0 0\n privilege level 15\n logging synchronous\n stopbits 1\nline aux 0\n exec-timeout 0 0\n privilege level 15\n logging synchronous\n stopbits 1\nline vty 0 4\n login\n!\n!\nend")
+            fichier.write(f"\n passive-interface {interface}")
+    fichier.write("\n!\n!\n!\n!\ncontrol-plane\n!\n!\nline con 0\n exec-timeout 0 0\n privilege level 15\n logging synchronous\n stopbits 1\nline aux 0\n exec-timeout 0 0\n privilege level 15\n logging synchronous\n stopbits 1\nline vty 0 4\n login\n!\n!\nend\n")
 
 def search_ip(r_name,v_name,liste_AS,AS_n) :
     for As in liste_AS :
@@ -121,15 +122,19 @@ def search_ip(r_name,v_name,liste_AS,AS_n) :
                             return router.ip[index]
     print("IP non trouvé")
     return None
-
+  
 ##############MAIN#############
+
 liste_AS = lecture_json()
 correspondance = {1 : "FastEthernet0/0", 2 : "GigabitEthernet1/0", 3 : "GigabitEthernet2/0", 4 : "GigabitEthernet3/0"}
 for As in liste_AS :
     for router in As.router :
-        with open(f"Config_test/i{router.name}_startup_config.txt", "w") as fichier:
+        with open(f"Config_finale/i{router.name}_startup-config.cfg", "w") as fichier:
             debut(fichier, router.name)
-            interfaces(fichier, router.ip, As.igp, router.name)
+            interface_border = []
+            for i in range(router.border[0]) :
+                interface_border.append(router.border[i+1])
+            interfaces(fichier, router.ip, As.igp,interface_border)
             neighbors_bgp = []
             passive_int = []
             index = 1
@@ -139,11 +144,9 @@ for As in liste_AS :
                         neighbors_bgp.append((search_ip(router.name,neighbor,liste_AS,router.border[i*2+2]),router.border[i*2+2]))
                         if As.igp == "OSPF" :
                             passive_int.append(correspondance[index])
-
-                    index+=1
+                index+=1
             for v_router in As.router :
                 if router != v_router :
                     neighbors_bgp.append((v_router.ip[4],As.number))
-            BGP(router.name,As.number,router.id,neighbors_bgp,As.prefixes)
-            IGP(router.name,router.id,As.igp,passive_int)
-
+            BGP(fichier,As.number,router.id,neighbors_bgp,As.prefixes)
+            IGP(fichier,router.id,As.igp,passive_int)
--- deplacement.py
+++ deplacement.py
@@ -2,51 +2,57 @@ import os
 import shutil
 
 # Dictionnaire des correspondances entre numéro et UUID
-nom_fichier : {
-    "1" : "39a09daf-6bbb-49d2-bdef-bdece8410620",
-    "2" : "fe0ba0e8-6a04-4df7-9313-3e6fae359fa8",
-    "3" : "a1c19efa-228a-48e5-8a1d-823304341d74",
-    "4" : "75775339-44a9-4691-b549-9a79b4a4bfeb",
-    "5" : "e77dcbd6-4836-468f-a13e-a217f926ebe3",
+nom_fichier = {
+    "1": "39a09daf-6bbb-49d2-bdef-bdece8410620",
+    "2": "fe0ba0e8-6a04-4df7-9313-3e6fae359fa8",
+    "3": "a1c19efa-228a-48e5-8a1d-823304341d74",
+    "4": "75775339-44a9-4691-b549-9a79b4a4bfeb",
+    "5": "e77dcbd6-4836-468f-a13e-a217f926ebe3",
     "6": "08ab2678-0b8c-4c30-8ed6-b98c4fdc7bb4",
     "7": "e832c8a7-6a47-445e-b387-e5131f76e1a7",
-    "8" : "e832c8a7-6a47-445e-b387-e5131f76e1a7",
-    "9" : "15c3dfa8-cb83-4f97-b3cc-dc12aefd0783",
-    "10" : "852a964a-91e6-4e41-8fb2-4cbb3fc387c8",
-    "11" : "6228e624-4df2-4960-bc5e-18c4b13a3902",
-    "12" : "ee6eb131-eaa9-4247-a5c4-5e0522e20f51",
-    "13" : "92fd3895-4db0-4412-8d46-d2cc9be695db",
-    "14" : "acdcac23-67fc-40b3-9fbe-a495b49e42f5"
+    "8": "e832c8a7-6a47-445e-b387-e5131f76e1a7",
+    "9": "15c3dfa8-cb83-4f97-b3cc-dc12aefd0783",
+    "10": "852a964a-91e6-4e41-8fb2-4cbb3fc387c8",
+    "11": "6228e624-4df2-4960-bc5e-18c4b13a3902",
+    "12": "ee6eb131-eaa9-4247-a5c4-5e0522e20f51",
+    "13": "92fd3895-4db0-4412-8d46-d2cc9be695db",
+    "14": "acdcac23-67fc-40b3-9fbe-a495b49e42f5"
 }
 
-# Chemin du dossier contenant les fichiers .txt
-chemin_dossier_source = 'C:/Users/pc/OneDrive/Documents/GNS3/GNS3_GRP_15/Config_test'  # Remplacez cela par votre chemin réel
+# Chemin du dossier contenant les fichiers .cfg
+chemin_dossier_source = 'C:/Users/pc/OneDrive/Documents/GNS3/GNS3_GRP_15/Config_finale'  # Remplacez cela par votre chemin réel
 
 # Chemin du dossier de destination principal
 chemin_dossier_destination = 'C:/Users/pc/OneDrive/Documents/projet gns3 fin TD2/projet gns3 fin TD2/project-files/dynamips'
 
+# Vérifier si le dossier de destination existe, sinon le créer
+if not os.path.exists(chemin_dossier_destination):
+    os.makedirs(chemin_dossier_destination)
+
 for nom_fic in os.listdir(chemin_dossier_source):
-    if nom_fic.endswith('.txt') and nom_fic.startswith('i'):
-        numero = nom_fic[1]         
+    if nom_fic.endswith('.cfg') and nom_fic.startswith('i'):
+        # Extraire le numéro du fichier en ignorant le préfixe "i" et l'extension ".cfg"
+        underscore_index = nom_fic.find('_')
+        numero = nom_fic[1:underscore_index] if underscore_index != -1 else nom_fic[1:-4]
+        print(f"Traitement du fichier {nom_fic} avec le numéro {numero}")
+
         if numero in nom_fichier:
             correspondance = nom_fichier[numero]
-            
+
             chemin_dossier_correspondant = os.path.join(chemin_dossier_destination, correspondance)
             chemin_configs = os.path.join(chemin_dossier_correspondant, 'configs')
+
+            # Vérifier si les dossiers existent, sinon les créer
+            if not os.path.exists(chemin_dossier_correspondant):
+                os.makedirs(chemin_dossier_correspondant)
+            if not os.path.exists(chemin_configs):
+                os.makedirs(chemin_configs)
+
             chemin_destination = os.path.join(chemin_configs, nom_fic)
-            
-            # Vérifier si le fichier existe déjà dans le dossier "configs" et le supprimer
-            if os.path.exists(chemin_destination):
-                try:
-                    os.remove(chemin_destination)
-                    print(f"Fichier existant à {chemin_destination} supprimé.")
-                except PermissionError as e:
-                    print(f"Impossible de supprimer le fichier existant à {chemin_destination}: {e}")
-            
+
             # Déplacer le fichier source vers le dossier "configs"
             try:
                 shutil.move(os.path.join(chemin_dossier_source, nom_fic), chemin_destination)
                 print(f"Fichier {nom_fic} déplacé et écrasé vers {chemin_destination}")
             except Exception as e:
                 print(f"Impossible de déplacer {nom_fic} vers {chemin_destination}: {e}")
-
